f4a622090a9229716fdc4af922acf44ffbd23363
erika2201 <erikapapamija@gmail.com>
2023-05-13 14:47:35 -0500

    Competencies test improved

--- dist/login.js
+++ dist/login.js
@@ -1976,7 +1976,7 @@ parcelHelpers.export(exports, "validateContextObject", ()=>validateContextObject
 parcelHelpers.export(exports, "validateIndexedDBOpenable", ()=>validateIndexedDBOpenable);
 parcelHelpers.export(exports, "validateNamespace", ()=>validateNamespace);
 var global = arguments[3];
-var process = require("7c980b15fdb56214");
+var process = require("3aef7a8b1b72cea6");
 const CONSTANTS = {
     /**
      * @define {boolean} Whether this is the client Node.js SDK.
@@ -3673,7 +3673,7 @@ function indicator(i) {
     else return service;
 }
 
-},{"7c980b15fdb56214":"d5jf4","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"d5jf4":[function(require,module,exports) {
+},{"3aef7a8b1b72cea6":"d5jf4","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"d5jf4":[function(require,module,exports) {
 // shim for using process in browser
 var process = module.exports = {};
 // cached from whatever global is present so that test runners that stub it
@@ -13890,7 +13890,7 @@ var _component = require("@firebase/component");
 var _logger = require("@firebase/logger");
 var _util = require("@firebase/util");
 var _webchannelWrapper = require("@firebase/webchannel-wrapper");
-var process = require("e91855bca6eb5e18");
+var process = require("aa13e2a2a9b9f1a7");
 const v = "@firebase/firestore";
 /**
  * @license
@@ -32845,7 +32845,7 @@ function kf(t, e) {
     (0, _app.registerVersion)(v, "3.10.1", "esm2017");
 }();
 
-},{"e91855bca6eb5e18":"d5jf4","@firebase/app":"3AcPV","@firebase/component":"bi1VB","@firebase/logger":"fZmft","@firebase/util":"ePiK6","@firebase/webchannel-wrapper":"yQiQ1","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"yQiQ1":[function(require,module,exports) {
+},{"aa13e2a2a9b9f1a7":"d5jf4","@firebase/app":"3AcPV","@firebase/component":"bi1VB","@firebase/logger":"fZmft","@firebase/util":"ePiK6","@firebase/webchannel-wrapper":"yQiQ1","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"yQiQ1":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "ErrorCode", ()=>ErrorCode);
